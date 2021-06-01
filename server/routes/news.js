512d407337f31b82e9a9818cef841bbd75fcdccf
Andrei Popica <popica.andreivlad@gmail.com>
2021-06-01 13:52:01 +0300

    headlines retrieve

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authorize = require('./permissions');
+const authorize = require("./permissions");
 
-router.get('/', authorize, (req, res) => {
-  res.send(req.user);
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI(process.env.API_KEY);
+
+router.get("/headlines", authorize, (req, res) => {
+
+  newsapi.v2
+    .topHeadlines({
+      category: req.query.category,
+      language: "en",
+    })
+    .then((content) => {
+      res.send(content);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
 });
 
 module.exports = router;
