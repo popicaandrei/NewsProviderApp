cbadfc343ad54392bc5152ce6ab5026a30c2ff89
Andrei Popica <popica.andreivlad@gmail.com>
2021-06-01 14:01:06 +0300

    search all news based on subject

--- server/routes/news.js
+++ server/routes/news.js
@@ -20,4 +20,20 @@ router.get("/headlines", authorize, (req, res) => {
     });
 });
 
+router.get("/subjects", authorize, (req, res) => {
+
+    newsapi.v2
+      .everything({
+        qInTitle: req.query.subject,
+        language: 'en',
+        sortBy: 'relevancy'
+      })
+      .then((content) => {
+        res.send(content);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  });
+
 module.exports = router;
